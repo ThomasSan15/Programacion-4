4ce230ac7865cb79c95607bbcfb0473db21a6ca8
Thomas Beltran <thomas.beltran@utp.edu.co>
2026-04-05 20:57:19 -0500

    Create gestor_menus.py with weekday mapping
    Added a dictionary to map numbers to weekdays.

---
+++ Proyecto_1_Desacoplamiento de software/gestor_menus.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+
+DIAS_SEMANA = {
+    0: "lunes",
+    1: "martes",
+    2: "miercoles",
+    3: "jueves",
+    4: "viernes",
+    5: "sabado",
+    6: "domingo",
+} # Diccionario que asigna cada número (0–6) a un día de la semana.
+
