a7f9f6317560bd8fa43efad501e136f6ebc613bc
Thomas Beltran <thomas.beltran@utp.edu.co>
2026-04-05 20:54:01 -0500

    Add Comensal class with discount calculation methods

--- Proyecto_1_Desacoplamiento de software/comensal.py
+++ Proyecto_1_Desacoplamiento de software/comensal.py
@@ -8,3 +8,26 @@ DESCUENTOS = {
     ("particular", "subsidiado_alto"):0.1,
     ("particular", "subsidiado_medio"):0.0,
 }
+
+
+class Comensal:
+    def __init__(self, id_cliente, nombre, _tipo_subsidio, _tipo_cliente):
+        self._id_cliente = id_cliente
+        self._nombre = nombre
+        self._tipo_subsidio = _tipo_subsidio
+        self._tipo_cliente = _tipo_cliente
+
+    def calcular_descuento(self, valor_plato):
+        porcentaje = DESCUENTOS.get((self._tipo_cliente, self._tipo_subsidio), 0.0)
+        return valor_plato * porcentaje
+
+    def precio_final(self, valor_plato):
+        return valor_plato - self.calcular_descuento(valor_plato)
+    
+    @property
+    def nombre(self):
+        return self._nombre
+    
+    @property
+    def id_cliente(self):
+        return self._id_cliente
