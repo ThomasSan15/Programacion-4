b857bf591f97a63108e596813af647838e9f9cda
Thomas Beltran <thomas.beltran@utp.edu.co>
2026-04-05 20:53:32 -0500

    Add discount rates for different user types

---
+++ Proyecto_1_Desacoplamiento de software/comensal.py
@@ -0,0 +1,10 @@
+DESCUENTOS = {
+    ("estudiante", "subsidiado_alto"):0.5,
+    ("estudiante", "subsidiado_medio"):0.3,
+    
+    ("administrativo","subsidiado_alto"):0.3,
+    ("administrativo","subsidiado_medio"):0.1,
+    
+    ("particular", "subsidiado_alto"):0.1,
+    ("particular", "subsidiado_medio"):0.0,
+}
